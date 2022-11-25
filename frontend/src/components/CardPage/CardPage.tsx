c521e1ec68953020cc2e1bdb2a11ce975ea98e94
sharkeeyy <sfmei2003@yandex.ru>
2022-11-25 12:29:38 +0300

    fixes-2

--- frontend/src/components/CardPage/CardPage.tsx
+++ frontend/src/components/CardPage/CardPage.tsx
@@ -15,25 +15,84 @@ function CardPage(): JSX.Element {
 
   function clickHandler(): void {
     if (que?.answer === value) {
-      setRight(true);
+      setTimeout(() => {
+        setRight(true);
+      }, 300);
       dispatch({ type: 'ADD_SCORE', payload: que?.price });
     } else {
-      setRight(false);
+      setTimeout(() => {
+        setRight(false);
+      }, 300);
     }
     dispatch({ type: 'DISABLE_Q', payload: que });
-    navigate('/main');
   }
 
   return (
-    <div className=" d-flex justify-content-center align-items-center w-100 h-100 m-5">
-    <div className="container border border-4 rounded p-3">
-    <div className="card-body">
-      <h1 className="card-title">{que?.theme}</h1>
-      <h2 className="card-text">{que?.name}</h2>
-      <input type="text" value={value} onChange={(e) => setValue(e.target.value)} />
-      <button type="button" onClick={clickHandler} className="btn btn-primary ">Your answer</button>
-    </div>
-    </div>
+    <div className="d-flex justify-content-center align-items-center w-100 h-100 m-5">
+      {isRight === 'none' && (
+        <div className="container border border-5 rounded p-3">
+          <div className="card-body">
+            <h1 className="card-title mb-1">{que?.theme}</h1>
+            <h3 className="card-text mb-3">{que?.name}</h3>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Your answer"
+                aria-label="Answer"
+                aria-describedby="basic-addon2"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+            <button type="button" onClick={clickHandler} className="btn btn-primary">Try it!</button>
+          </div>
+        </div>
+      )}
+      {isRight === true && (
+        <div className="container border border-5 border-success rounded p-3">
+          <div className="card-body">
+            <h1 className="card-title mb-1">{que?.theme}</h1>
+            <h3 className="card-text mb-3">{que?.name}</h3>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Your answer"
+                aria-label="Answer"
+                aria-describedby="basic-addon2"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+            <button type="button" onClick={clickHandler} className="btn btn-primary">Try it!</button>
+            <p className="text-success">Правильно!</p>
+            <button type="button" className="btn btn-info" onClick={() => navigate('/main')}>Next</button>
+          </div>
+        </div>
+      )}
+      {isRight === false && (
+        <div className="container border border-5 rounded border-danger p-3">
+          <div className="card-body">
+            <h1 className="card-title mb-1">{que?.theme}</h1>
+            <h3 className="card-text mb-3">{que?.name}</h3>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Your answer"
+                aria-label="Answer"
+                aria-describedby="basic-addon2"
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </div>
+            <button type="button" onClick={clickHandler} className="btn btn-primary">Try it!</button>
+            <p className="text-danger">Не угадали!</p>
+            <button type="button" className="btn btn-info" onClick={() => navigate('/main')}>Next</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
